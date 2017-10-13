edb4257ab3ca13b337bab82f1cb99ba17de3a96a
gyt95 <gyt95@163.com>
2017-10-13 19:00:51 +0800

    Modify something

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,5 +1,8 @@
 import React,{ Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import './search.scss';
+import '../Header/header.scss';
 
 class Search extends Component{
     constructor(props){
@@ -21,37 +24,47 @@ class Search extends Component{
     }
     render(){
         return(
-            <div className="search-box">
-                <div className="hot-search">
-                    <span>热门搜索</span>
-                    <ul>
-                        <li>王者荣耀</li>
-                        <li>口袋妖怪</li>
-                        <li>梦幻西游</li>
-                        <li>阴阳师</li>
-                        <li>火影忍者</li>
-                        <li>部落冲突</li>
-                    </ul>
+            <div>
+                <div className="search-header">
+                    <Link to="/" className="head-left">&lt;</Link>
+                    <Link to="/search" className="search-input">
+                        <input type="text" placeholder="我的世界"/>
+                    </Link>
+                    <i className="iconfont icon-sousuo"></i>
                 </div>
-                <div className="history-search" ref="history">
-                    <span>历史搜索</span>
-                    <ul>
-                        <li>王者荣耀</li>
-                        <li>王者荣耀</li>
-                        <li>王者荣耀</li>
-                    </ul>
-                </div>
-                <div className="clean-history" onClick = { this.modalShow }>
-                    清空历史记录
-                </div>
-                <div className="mask" ref="mask"></div>
-                <div className="modal" ref="modal">
-                    <p>清空</p>
-                    <span>是否清空历史记录？</span>
-                    <div className="clean-btn">
-                        <input type="submit" value="取消" onClick = { this.modalHide } />
-                        <input type="submit" value="确定" onClick = { this.historyDel }/>
-                    </div>  
+                <div className="search-box">
+                    
+                    <div className="hot-search">
+                        <span>热门搜索</span>
+                        <ul>
+                            <li>王者荣耀</li>
+                            <li>口袋妖怪</li>
+                            <li>梦幻西游</li>
+                            <li>阴阳师</li>
+                            <li>火影忍者</li>
+                            <li>部落冲突</li>
+                        </ul>
+                    </div>
+                    <div className="history-search" ref="history">
+                        <span>历史搜索</span>
+                        <ul>
+                            <li>王者荣耀</li>
+                            <li>王者荣耀</li>
+                            <li>王者荣耀</li>
+                        </ul>
+                    </div>
+                    <div className="clean-history" onClick = { this.modalShow }>
+                        清空历史记录
+                    </div>
+                    <div className="mask" ref="mask"></div>
+                    <div className="modal" ref="modal">
+                        <p>清空</p>
+                        <span>是否清空历史记录？</span>
+                        <div className="clean-btn">
+                            <input type="submit" value="取消" onClick = { this.modalHide } />
+                            <input type="submit" value="确定" onClick = { this.historyDel }/>
+                        </div>  
+                    </div>
                 </div>
             </div>
         )
